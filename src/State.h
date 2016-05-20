29d42ac84c52b112a91bc53f14b8b3b29258d12a
daotranminh <daotranminh@gmail.com>
2016-05-20 22:54:53 +0200

    Some experiment

--- src/State.h
+++ src/State.h
@@ -9,5 +9,19 @@
 #ifndef playground_State_h
 #define playground_State_h
 
+#include "rxcpp/rx.hpp"
+#include "RDFtextFileReader.h"
+
+struct State
+{
+  rxcpp::subjects::subject<RDFtext> data_subject_;
+  RDFtextFileReader reader_;
+  
+  bool init_reading_done_;
+  
+  State()
+  : init_reading_done_{false}
+  { }
+};
 
 #endif
--- src/SubscriptionPoint.h
+++ src/SubscriptionPoint.h
@@ -11,6 +11,9 @@
 
 #include <memory>
 
+#include "rxcpp/rx.hpp"
+
+#include "RDFtext.h"
 #include "State.h"
 
 class SubscriptionPoint
@@ -23,6 +26,9 @@ public:
   void
   initialize_reading();
   
+  rxcpp::observable<RDFtext>
+  create_observable();
+  
 private:
   std::shared_ptr<State> state_;
 };
@@ -31,7 +37,39 @@ private:
 void
 SubscriptionPoint::initialize_reading()
 {
-  
+  if (!state_->init_reading_done_)
+  {
+    typename rxcpp::subjects::subject<RDFtext>::subscriber_type subscriber =
+    state_->data_subject_.get_subscriber();
+    
+    auto samples = state_->reader_.take();
+    for (auto sample : samples)
+      subscriber.on_next(sample);
+    
+    state_->init_reading_done_ = true;
+  }
 }
+
+rxcpp::observable<RDFtext>
+SubscriptionPoint::create_observable()
+{
+  SubscriptionPoint me = *this;
+  
+  return rxcpp::observable<>::create<RDFtext>
+  (
+   [me](rxcpp::subscriber<RDFtext> subscriber){
+     //const_cast<SubscriptionPoint&>(me).initialize_reading();
+     
+     auto samples = me.state_->reader_.take();
+     for (auto sample : samples)
+       subscriber.on_next(sample);
+     
+     /*rxcpp::composite_subscription subscription =
+       me.state_->data_subject_.get_observable().subscribe(subscriber);
+     
+     return subscription;*/
+   });
+}
+
 
 #endif
