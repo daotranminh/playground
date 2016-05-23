dc46be59e58a6d30e2980aa8b70d181ca6bb077c
daotranminh <daotranminh@gmail.com>
2016-05-23 14:23:01 +0200

    Use subject to create input stream

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,14 @@
 #include <clocale>
 
 #include <iostream>
+#include <string>
 #include <vector>
 
 #include "RDFtextFileReader.h"
 
 int main()
 {
-  rxcpp::observable<RDFtext> input_stream =
+/*  rxcpp::observable<RDFtext> input_stream =
   rxcpp::observable<>::create<RDFtext>(
     [](rxcpp::subscriber<RDFtext> subscriber)
     {
@@ -24,6 +25,22 @@ int main()
   (
    [](const RDFtext &triple){ std::cout << "On next " << triple << std::endl; },
    [](){ std::cout << "On complete" << std::endl; }
+  );*/
+  
+  rxcpp::subjects::subject<std::string> subj;
+  
+  rxcpp::subscriber<std::string> sub = subj.get_subscriber();
+  rxcpp::observable<std::string> obs = subj.get_observable();
+  
+  obs.subscribe
+  (
+   [](std::string s){ std::cout << "On next with " << s << std::endl; },
+   [](){ std::cout << "On completed here!" << std::endl;}
   );
+  
+  sub.on_next("aaa");
+  sub.on_next("bbb");
+  sub.on_next("ccc");
+  sub.on_completed();
 }
 
